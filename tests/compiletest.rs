2d2210744c521b233aad46e920b58bf4c6483a06
Taiki Endo <te316e89@gmail.com>
2020-10-30 03:15:31 +0900

    Update macrotest and remove scripts/expandtest.sh

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -1,8 +1,14 @@
 #![warn(rust_2018_idioms, single_use_lifetimes)]
 
+use std::env;
+
 #[rustversion::attr(not(nightly), ignore)]
 #[test]
 fn ui() {
+    if !env::var_os("CI").map_or(false, |v| v == "true") {
+        env::set_var("TRYBUILD", "overwrite");
+    }
+
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/ui/*.rs");
 }
