7e00ff709e6740d06f6f92d3b35cad358aa43945
Taiki Endo <te316e89@gmail.com>
2020-11-08 05:38:04 +0900

    Simplify CI environment variable check

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -5,7 +5,7 @@ use std::env;
 #[rustversion::attr(not(nightly), ignore)]
 #[test]
 fn ui() {
-    if !env::var_os("CI").map_or(false, |v| v == "true") {
+    if env::var_os("CI").is_none() {
         env::set_var("TRYBUILD", "overwrite");
     }
 
