64bba51714b2935bf7f1361df4d6461d4aed7b60
Taiki Endo <te316e89@gmail.com>
2020-12-23 01:28:59 +0900

    Update allowed lints

--- src/lib.rs
+++ src/lib.rs
@@ -105,8 +105,6 @@
 #![warn(unsafe_code)]
 #![warn(future_incompatible, rust_2018_idioms, single_use_lifetimes, unreachable_pub)]
 #![warn(clippy::all, clippy::default_trait_access)]
-// mem::take requires Rust 1.40
-#![allow(clippy::mem_replace_with_default)]
 
 /// A macro that creates a projection type covering all the fields of struct.
 ///
