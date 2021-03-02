e8af0fb6433d28b39e17af6f3b3ee7ef58105ba2
Taiki Endo <te316e89@gmail.com>
2021-03-02 21:00:24 +0900

    Enable CI on v0.1 branch
    Also, this updates CI config based on master branch.

--- scripts/check-minimal-versions.sh
+++ scripts/check-minimal-versions.sh
@@ -3,56 +3,70 @@
 # Check all public crates with minimal version dependencies.
 #
 # Usage:
-#    bash scripts/check-minimal-versions.sh [check|test]
+#    ./scripts/check-minimal-versions.sh [+toolchain] [check|test] [options]
 #
 # Note:
 # - This script modifies Cargo.toml and Cargo.lock while running
-# - This script exits with 1 if there are any unstaged changes
-# - This script requires nightly Rust and cargo-hack
+# - This script exits with 1 if there are any unstaged changes on Cargo.toml
+# - This script requires nightly toolchain and cargo-hack
 #
 # Refs: https://github.com/rust-lang/cargo/issues/5657
 
 set -euo pipefail
+IFS=$'\n\t'
+
+error() {
+    if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
+        echo "::error::$*"
+    else
+        echo "error: $*" >&2
+    fi
+}
 
 cd "$(cd "$(dirname "${0}")" && pwd)"/..
 
 # Decide Rust toolchain.
-# If the `CI` environment variable is not set to `true`, then nightly is used by default.
-if [[ "${1:-none}" == "+"* ]]; then
+# Nightly is used by default if the `CI` environment variable is unset.
+if [[ "${1:-}" == "+"* ]]; then
     toolchain="${1}"
     shift
-elif [[ "${CI:-false}" != "true" ]]; then
-    cargo +nightly -V >/dev/null || exit 1
+elif [[ -z "${CI:-}" ]]; then
     toolchain="+nightly"
 fi
-# This script requires nightly Rust and cargo-hack
+# Make sure toolchain is installed.
+cargo ${toolchain:-} -V >/dev/null
 if [[ "${toolchain:-+nightly}" != "+nightly"* ]] || ! cargo hack -V &>/dev/null; then
-    echo "error: check-minimal-versions.sh requires nightly Rust and cargo-hack"
+    error "this script requires nightly toolchain and cargo-hack"
     exit 1
 fi
 
-subcmd="${1:-check}"
-case "${subcmd}" in
-    check | test) ;;
-    *)
-        echo "error: invalid argument \`${1}\`"
-        exit 1
-        ;;
-esac
+# Decide subcommand.
+subcmd="check"
+if [[ "${1:-}" =~ ^(check|test)$ ]]; then
+    subcmd="${1}"
+    shift
+fi
 
-# This script modifies Cargo.toml and Cargo.lock, so make sure there are no
-# unstaged changes.
-git diff --exit-code
+# This script modifies Cargo.toml, so make sure there are no unstaged changes
+# on Cargo.toml.
+# shellcheck disable=SC2046
+git diff --exit-code $(git ls-files "*Cargo.toml")
 # Restore original Cargo.toml and Cargo.lock on exit.
-trap 'git checkout .' EXIT
+trap 'git checkout $(git ls-files "*Cargo.toml")' EXIT
 
 if [[ "${subcmd}" == "check" ]]; then
     # Remove dev-dependencies from Cargo.toml to prevent the next `cargo update`
     # from determining minimal versions based on dev-dependencies.
-    cargo hack --remove-dev-deps --workspace
+    (
+        set -x
+        cargo hack --remove-dev-deps --workspace
+    )
 fi
 
-# Update Cargo.lock to minimal version dependencies.
-cargo ${toolchain:-} update -Zminimal-versions
-# Run check for all public members of the workspace.
-cargo ${toolchain:-} hack "${subcmd}" --workspace --all-features --ignore-private -Zfeatures=all
+(
+    set -x
+    # Update Cargo.lock to minimal version dependencies.
+    cargo ${toolchain:-} update -Z minimal-versions
+    # Run check for all public members of the workspace.
+    cargo ${toolchain:-} hack "${subcmd}" --workspace --all-features --ignore-private "$@"
+)
--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,37 +1,66 @@
 #!/bin/bash
 
-# A script to run a simplified version of the checks done by CI.
+# Run a simplified version of the checks done by CI.
 #
 # Usage:
-#     bash scripts/ci.sh
+#     ./scripts/ci.sh [+toolchain]
 #
 # Note: This script requires nightly Rust, rustfmt, clippy, and cargo-expand
 
 set -euo pipefail
+IFS=$'\n\t'
 
-if [[ "${1:-none}" == "+"* ]]; then
+error() {
+    echo "error: $*" >&2
+}
+warn() {
+    echo "warning: $*" >&2
+}
+
+# Decide Rust toolchain. Nightly is used by default.
+toolchain="+nightly"
+if [[ "${1:-}" == "+"* ]]; then
     toolchain="${1}"
-else
-    cargo +nightly -V >/dev/null || exit 1
-    toolchain="+nightly"
+    shift
+fi
+# Make sure toolchain is installed.
+if ! cargo "${toolchain}" -V &>/dev/null; then
+    rustup toolchain install "${toolchain#+}" --no-self-update --profile minimal
 fi
 
-if [[ "${toolchain:-+nightly}" != "+nightly"* ]] || ! rustfmt -V &>/dev/null || ! cargo clippy -V &>/dev/null || ! cargo expand -V &>/dev/null; then
-    echo "error: ci.sh requires nightly Rust, rustfmt, clippy, and cargo-expand"
+if [[ "${toolchain:-+nightly}" != "+nightly"* ]]; then
+    error "ci.sh requires nightly Rust"
     exit 1
 fi
+if ! rustup "${toolchain}" component add rustfmt &>/dev/null \
+    || ! cargo expand -V &>/dev/null; then
+    warn "ci.sh requires rustfmt and cargo-expand to run all tests"
+fi
 
-echo "Running 'cargo ${toolchain} fmt --all'"
-cargo "${toolchain}" fmt --all
+# Run rustfmt.
+if ! rustup "${toolchain}" component add rustfmt &>/dev/null; then
+    warn "component 'rustfmt' is unavailable for toolchain '${toolchain#+}'"
+else
+    (
+        set -x
+        cargo "${toolchain}" fmt --all
+    )
+fi
 
-echo "Running 'cargo ${toolchain} clippy --all --all-targets'"
-cargo "${toolchain}" clippy --all --all-features --all-targets -Zunstable-options
+# Run clippy.
+if ! rustup "${toolchain}" component add clippy &>/dev/null; then
+    warn "component 'clippy' is unavailable for toolchain '${toolchain#+}'"
+else
+    (
+        set -x
+        cargo "${toolchain}" clippy --all --all-features --all-targets -Z unstable-options
+    )
+fi
 
-echo "Running 'cargo ${toolchain} test --all --exclude expandtest'"
-TRYBUILD=overwrite cargo "${toolchain}" test --all --all-features --exclude expandtest
+set -x
 
-echo "Running 'bash scripts/expandtest.sh ${toolchain}'"
-"$(cd "$(dirname "${0}")" && pwd)"/expandtest.sh "${toolchain}"
-
-echo "Running 'cargo ${toolchain} doc --no-deps --all'"
+# Build documentation.
 cargo "${toolchain}" doc --no-deps --all --all-features
+
+# Run tests.
+cargo "${toolchain}" test --all --all-features
